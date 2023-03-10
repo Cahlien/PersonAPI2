c9d06f801eb422455cf6df0a20125b5d11fff0f2
Cahlien <78377986+Cahlien@users.noreply.github.com>
2023-03-09 19:55:54 -0600

    enable cors again

---
+++ src/main/java/dev/crowell/personapi2/configurations/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package dev.crowell.personapi2.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .csrf().disable()
+                .cors();
+        return http.build();
+
+    }
+}
